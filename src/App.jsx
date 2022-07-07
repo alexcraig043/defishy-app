88f8e99b1b01e6d0d3589fc437d676ae1e541b43
alexcraig <alexcraig043@gmail.com>
2022-07-07 15:45:06 -0600

    Darkmode to local storage and started some firebase stuff

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,45 @@ import Login from "./pages/login/Login.jsx";
 import List from "./pages/list/List.jsx";
 import Single from "./pages/single/Single.jsx";
 import New from "./pages/new/New.jsx";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { productInputs, userInputs } from "./formSource.js";
 import "./style/dark.scss";
 import { useContext } from "react";
 import { DarkModeContext } from "./context/darkModeContext";
+import { AuthContext } from "./context/authContext.js";
 
 function App() {
   const { darkMode } = useContext(DarkModeContext);
+
+  const { currentUser } = useContext(AuthContext);
+
+  const RequireAuth = ({ children }) => {
+    return currentUser ? children : <Navigate to="/login" />;
+  };
 
   return (
     <div className={darkMode ? "app dark" : "app"}>
       <BrowserRouter>
         <Routes>
           <Route path="/">
-            <Route index element={<Home />} />
             <Route path="login" element={<Login />} />
+            <Route
+              index
+              element={
+                <RequireAuth>
+                  <Home />
+                </RequireAuth>
+              }
+            />
             <Route path="users">
-              <Route index element={<List />} />
+              <Route
+                index
+                element={
+                  <RequireAuth>
+                    <List />
+                  </RequireAuth>
+                }
+              />
               <Route path=":userId" element={<Single />} />
               <Route
                 path="new"
@@ -28,11 +49,29 @@ function App() {
               />
             </Route>
             <Route path="products">
-              <Route index element={<List />} />
-              <Route path=":productId" element={<Single />} />
+              <Route
+                index
+                element={
+                  <RequireAuth>
+                    <List />
+                  </RequireAuth>
+                }
+              />
+              <Route
+                path=":productId"
+                element={
+                  <RequireAuth>
+                    <Single />
+                  </RequireAuth>
+                }
+              />
               <Route
                 path="new"
-                element={<New inputs={productInputs} title="Add New Product" />}
+                element={
+                  <RequireAuth>
+                    <New inputs={productInputs} title="Add New Product" />
+                  </RequireAuth>
+                }
               />
             </Route>
           </Route>
--- src/context/darkModeContext.js
+++ src/context/darkModeContext.js
@@ -1,14 +1,19 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useEffect } from "react";
 import DarkModeReducer from "./darkModeReducer";
 
 const INITIAL_STATE = {
-  darkMode: false,
+  darkMode: JSON.parse(localStorage.getItem("darkMode") || false),
 };
 
 export const DarkModeContext = createContext(INITIAL_STATE);
 
 export const DarkModeContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(DarkModeReducer, INITIAL_STATE);
+
+  useEffect(() => {
+    localStorage.setItem("darkMode", JSON.stringify(state.darkMode));
+    console.log(state.darkMode);
+  }, [state.darkMode]);
 
   return (
     <DarkModeContext.Provider value={{ darkMode: state.darkMode, dispatch }}>
